c23104078d047728312d52d63fd52512e00c3f64
Ganesh Khadsan <ganeshskhadsan@gmail.com>
2019-01-15 15:28:23 +0530

    edit , update, delete

--- todolist/app/controllers/items_controller.rb
+++ todolist/app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
 
+    before_action :find_item, only: [:show, :edit, :update, :destroy]
+
     def index
         @items = Item.all.order("created_at DESC")
     end    
@@ -8,6 +10,25 @@ class ItemsController < ApplicationController
         @item = Item.new
     end
     
+    def show
+       
+    end
+    
+    def edit
+        
+    end
+
+    def update
+        if @item.update(item_params)
+          flash[:success] = "Task was successfully updated"
+          redirect_to item_path(@item)
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+    
+
     def create
         @item = Item.new(item_params)
         if @item.save
@@ -18,10 +39,26 @@ class ItemsController < ApplicationController
           render 'new'
         end
     end
+
+    def destroy
+        if @item.destroy
+            flash[:success] = 'Object was successfully deleted.'
+            redirect_to root_path
+        else
+            flash[:error] = 'Something went wrong'
+            redirect_to root_path
+        end
+    end
+    
+    
     
 private
 def item_params 
     params.require(:item).permit(:title , :description)
+end
+
+def find_item
+    @item = Item.find(params[:id])
 end
 
 end
